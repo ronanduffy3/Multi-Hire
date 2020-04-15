95da1f05215adc5a63196c99a9d4518755a2cd9c
Ronan Duffy <s00187127@mail.itsligo.ie>
2020-04-15 15:27:35 +0100

    added functionality to add a hire asset to a database and tidied up code in parts

--- HireMockup/MainWindow.xaml.cs
+++ HireMockup/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,12 +25,12 @@ namespace HireMockup
     {
         //Create a connection to the DB
         Model1Container db = new Model1Container();
-        
+
 
         public MainWindow()
         {
             InitializeComponent();
-
+            
 
 
         }
@@ -55,7 +58,10 @@ namespace HireMockup
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            
+            string hireName = tbx_addHireName.Text.ToString();
+            string hireType = tbx_addHireType.Text.ToString();
+            decimal hireDailyRate = Decimal.Parse(tbx_addHireDailyRate.Text);
+            newHireAsset(hireName, hireType, hireDailyRate);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
@@ -70,7 +76,7 @@ namespace HireMockup
             //        HireID = txtbox_newHireID.Text,
             //        HireName = txtbox_newHireName.Text,
             //        DailyRate = dailyRate                   
-                                        
+
             //    };
             //    HireList.Add(FormHireItem);
             //    lbx_homePage.Items.Refresh();
@@ -117,6 +123,54 @@ namespace HireMockup
             var results = query.ToList();
 
             dataGrid_Home.ItemsSource = results;
+        }
+
+        private void dataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from em in db.Employees
+                        select em;
+
+            var result = query.ToList();
+
+            employeeDataGrid.ItemsSource = result;
+
+            totalSalaryCalculation();
+
+        }
+
+        // Method to set/update total salary calculation that we can can when the form loads and when a new employee is created
+        public void totalSalaryCalculation()
+        {
+            var query2 = from em in db.Employees
+                         select em.weeklySalary;
+
+            decimal totalWeeklySalaryBill = query2.Sum();
+
+            lbl_wageTotal.Content = totalWeeklySalaryBill.ToString("C2");
+        }
+
+        [Obsolete]
+        public void newHireAsset(string hireName, string hireType, decimal dailyRate)
+        {
+            try
+            {
+                SqlConnection sql = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\DATABASES\MULTIHIREDB.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                sql.Open();
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO HireAssets (hireName, hireType, dailyRate) VALUES (@Name, @Type, @dailyRate)", sql);
+                sqlCommand.Parameters.Add("@Name", hireName);
+                sqlCommand.Parameters.Add("@Type", hireType);
+                sqlCommand.Parameters.Add("@dailyRate", dailyRate);
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Completed correctly");
+            }
+
         }
     }
 }
